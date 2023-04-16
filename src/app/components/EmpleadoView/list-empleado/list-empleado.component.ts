35a1736a8edfba0eaff00231516860acda386d4e
marcelino heredia <marcelinoheredia_02@hotmail.com>
2023-04-16 12:08:14 -0400

    change table empleado

--- src/app/components/EmpleadoView/list-empleado/list-empleado.component.ts
+++ src/app/components/EmpleadoView/list-empleado/list-empleado.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Empleado } from 'src/app/models/Empleado';
+import { Sexo } from 'src/app/models/Sexo';
 import { empleadoRepository } from 'src/app/services/empleadoRepository';
+import { sexoRepository } from 'src/app/services/sexoRepository';
 
 @Component({
   selector: 'app-list-empleado',
@@ -11,14 +13,23 @@ export class ListEmpleadoComponent {
 
    service:empleadoRepository = new empleadoRepository();
    listEmpleado:Empleado[] = [];
+   sexoRepository:sexoRepository = new sexoRepository();
+   listSexo:Sexo[] = [];
 
    ngOnInit() : void {
+    this.listSexo = [...this.sexoRepository.GetAll()];
     this.listEmpleado = this.service.GetAll();
     console.log(this.listEmpleado);
    }
 
    DeleteEmpleado(id:number) {
      this.listEmpleado = [...this.service.Delete(id)]
+   }
+
+   GetSexo(id:number):string{
+    const item =  this.listSexo.find(x => x.sexoId === +id);
+    let descripcion = item === undefined? '' : item.descripcion;
+    return descripcion;
    }
 
 
